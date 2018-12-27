a334d9454689ee6f95b936725981644597337ffd
camelia-c <camelie_1985@yahoo.com>
2018-12-27 02:08:17 +0200

    Added setup.py
    to allow pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='callgraph',
+      version='1.0.0',
+      description='Callgraph is a Python package that defines a decorator, and Jupyter magic, to draw dynamic call graphs of Python function calls',
+      url_original='https://github.com/osteele/callgraph',
+      author='oliversteele',
+      author_email='',
+      license='MIT',
+      packages=['callgraph'],
+      zip_safe=False)
+
